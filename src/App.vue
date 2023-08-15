869e346dadbf41a64e9075be115f3e23e848edef
Mortivarus <58182213+Mortivarus@users.noreply.github.com>
2023-08-15 12:46:21 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,74 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+let prodName = ["Brood", "Broccoli", "Krentenbollen", "Noten"]
+let prodPrice = [1.00, 0.99, 1.20, 2.99]
+let prodQuantity = []
+
+
+
+
+
+
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+<table>
+        <tr>
+            <th>Product</th>
+            <th>Prijs</th>
+            <th>Aantal</th>
+            <th>Subtotaal</th>
+        </tr>
+        <tr>
+            <td>Brood</td>
+            <td class="productPrice" id="number">1.00</td>
+            <td><input type="number" class="productQuantity"></td>
+            <td class="productTotalCost" id="number">0</td>
+        </tr>
+        <tr>
+            <td>Broccoli</td>
+            <td class="productPrice" id="number">0.99</td>
+            <td><input type="number" class="productQuantity"></td>
+            <td class="productTotalCost" id="number">0</td>
+        </tr>
+        <tr>
+            <td>Krentenbollen</td>
+            <td class="productPrice" id="number">1.20</td>
+            <td><input type="number" class="productQuantity"></td>
+            <td class="productTotalCost" id="number">0</td>
+        </tr>
+        <tr>
+            <td>Noten</td>
+            <td class="productPrice" id="number">2.99</td>
+            <td><input type="number" class="productQuantity"></td>
+            <td class="productTotalCost" id="number">0</td>
+        </tr>
+        <tr>
+            <td colspan="3" id="total">Totaal</td>
+            <td id="totalCost">0</td>
+        </tr>
+    </table>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+table{
+    display: inline-table;
+    border-collapse: collapse;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+
+th, td, table{
+    border: 1px solid #888;
+    padding: 10px;
+    font-family: sans-serif;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+th{
+    color: white;
+    background-color: black;
+    text-align: left;
+}
+
+#number, #totalCost, #total{
+    text-align: right;
 }
 </style>
