59d3694f8a9cdb2cacc6bba9d2023a193dd97285
Mortivarus <58182213+Mortivarus@users.noreply.github.com>
2023-08-24 15:05:11 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,26 @@
 <script setup>
+import {ref, reactive, computed} from 'vue'
+
+
+
 let prodName = ["Brood", "Broccoli", "Krentenbollen", "Noten"]
 let prodPrice = [1.00, 0.99, 1.20, 2.99]
-let prodQuantity = []
+let prodQuantity = ref([0, 0, 0, 0])
+
+let prodTotals =  computed(()=>{
+    let list = []
+    for (let i = 0; i < prodName.length; i++) {
+        list[i] = (prodPrice[i] * prodQuantity.value[i])
+    }
+    return list
+})
 
 
 
-
-
-
+let sumTotal = computed(()=>{
+    const reducer = (accumulator, item) => {return accumulator + item}
+    return prodTotals.value.reduce(reducer, 0).toFixed(2)
+})
 
 </script>
 
@@ -21,31 +34,31 @@ let prodQuantity = []
         </tr>
         <tr>
             <td>Brood</td>
-            <td class="productPrice" id="number">1.00</td>
-            <td><input type="number" class="productQuantity"></td>
-            <td class="productTotalCost" id="number">0</td>
+            <td class="productPrice" id="number">{{ prodPrice[0].toFixed(2) }}</td>
+            <td><input type="number" class="productQuantity" v-model="prodQuantity[0]"></td>
+            <td class="productTotalCost" id="number">{{ prodTotals[0].toFixed(2)}}</td>
         </tr>
         <tr>
             <td>Broccoli</td>
-            <td class="productPrice" id="number">0.99</td>
-            <td><input type="number" class="productQuantity"></td>
-            <td class="productTotalCost" id="number">0</td>
+            <td class="productPrice" id="number">{{ prodPrice[1].toFixed(2) }}</td>
+            <td><input type="number" class="productQuantity" v-model="prodQuantity[1]"></td>
+            <td class="productTotalCost" id="number">{{ prodTotals[1].toFixed(2)}}</td>
         </tr>
         <tr>
             <td>Krentenbollen</td>
-            <td class="productPrice" id="number">1.20</td>
-            <td><input type="number" class="productQuantity"></td>
-            <td class="productTotalCost" id="number">0</td>
+            <td class="productPrice" id="number">{{ prodPrice[2].toFixed(2) }}</td>
+            <td><input type="number" class="productQuantity" v-model="prodQuantity[2]"></td>
+            <td class="productTotalCost" id="number">{{ prodTotals[2].toFixed(2)}}</td>
         </tr>
         <tr>
             <td>Noten</td>
-            <td class="productPrice" id="number">2.99</td>
-            <td><input type="number" class="productQuantity"></td>
-            <td class="productTotalCost" id="number">0</td>
+            <td class="productPrice" id="number">{{ prodPrice[3].toFixed(2) }}</td>
+            <td><input type="number" class="productQuantity" v-model="prodQuantity[3]"></td>
+            <td class="productTotalCost" id="number">{{ prodTotals[3].toFixed(2)}}</td>
         </tr>
         <tr>
             <td colspan="3" id="total">Totaal</td>
-            <td id="totalCost">0</td>
+            <td id="totalCost">{{ sumTotal }}</td>
         </tr>
     </table>
 </template>
